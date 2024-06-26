ffe4b15d78fc4810f1d076617514731a9ff1bf8e
greypixel <greypixel@metalex.tech>
2024-06-26 23:31:07 +0100

    Standard vote and veto interfaces

---
+++ src/interfaces/IVetoImplant.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.20;
+
+import "./IBaseImplant.sol";
+
+interface IVetoImplant is IBaseImplant {
+    event GovernanceExecutorUpdated(address governanceExecutor);
+
+    error GovernanceExecutorEmpty();
+    error CallerNotGovernance();
+
+    function governanceExecutor() external view returns (address);
+    function deleteProposal(uint256 _proposalId) external;
+    function setGovernanceExecutor(address _governanceExecutor) external;
+}
